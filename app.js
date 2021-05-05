d1af415e74055e71b46f0c2468ab564a91d2b3b1
MoriaiMamoru <ne280073@sesnhu-u.jp>
2021-05-05 17:09:04 +0900

    New Project

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res, next){
+    res.render("index", {});
+});
+
+/*
 var text = document.getElementById('text');
 
 var textLists = [
@@ -22,4 +36,5 @@ window.addEventListener("keydown", (e)=>{
     word.shift();
   }
 });
+*/
 
