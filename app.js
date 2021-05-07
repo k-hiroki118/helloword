07628c3e196b2755a573ee27f9fa5ab4cdd69f3e
h_kinjo <h_kinjo@style-free.co.jp>
2021-05-07 17:57:13 +0900

    app.js反映

--- app.js
+++ app.js
@@ -11,33 +11,8 @@ app.get("/", function(req, res, next){
     res.render("index", {});
 });
 
-var server = app.listen(3000, function(){
-  console.log("Node.js is listening to PORT:" + server.address().port);
-});
-/*
-var text = document.getElementById('text');
-
-var textLists = [
-  "helloworld"
-];
-
-var word = [];
-word =textLists[0].split("").map(function(value){
-  var span = document.createElement('span');
-  span.textContent = value;
-  text.appendChild(span);
-  return span;
+var server = app.listen(3001, function(){
+  console.log("Node.js is listening to URL: ( http://localhost:" + server.address().port + ' )');
 });
 
-word[0].className ="under-line";
-window.addEventListener("keydown", (e)=>{
-  var keycd = e.key;
-  if (word[0].innerHTML == keycd) {
-    word[0].className = "red";
-    word[1].className ="under-line";
-    text.insertBefore(word[0], word[1]);
-    word.shift();
-  }
-});
-*/
 
