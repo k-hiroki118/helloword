543c245fb52d0754c26c0d469b5ee233515f55a9
h_kinjo <h_kinjo@style-free.co.jp>
2021-05-08 17:40:17 +0900

    express-generatorの導入

---
+++ public/javascripts/app.js
@@ -0,0 +1,24 @@
+var text = document.getElementById('text');
+
+var textLists = [
+  "helloword."
+];
+
+var word = [];
+word =textLists[0].split("").map(function(value){
+  var span = document.createElement('span');
+  span.textContent = value;
+  text.appendChild(span);
+  return span;
+});
+
+word[0].className ="under-line";
+window.addEventListener("keydown", (e)=>{
+  var keycd = e.key;
+  if (word[0].innerHTML == keycd) {
+    word[0].className = "red";
+    word[1].className ="under-line";
+    text.insertBefore(word[0], word[1]);
+    word.shift();
+  }
+});
